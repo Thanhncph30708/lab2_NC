2fa4f009e22e5e4069ac97c9c4b3e3bd2ba67e9f
Thanhncph30708 <nguyenchithanh21052002@gmail.com>
2024-07-08 15:11:07 +0700

    Initial commit

---
+++ app/src/main/java/thanhncph30708/fpoly/lab2_nc/Item.java
@@ -0,0 +1,37 @@
+package thanhncph30708.fpoly.lab2_nc;
+
+public class Item {
+    private long id;
+    private String itemName;
+    private int age;
+
+    public Item(long id, String itemName, int age) {
+        this.id = id;
+        this.itemName = itemName;
+        this.age = age;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public String getItemName() {
+        return itemName;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public void setItemName(String itemName) {
+        this.itemName = itemName;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+}
